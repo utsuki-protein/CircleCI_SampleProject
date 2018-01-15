9751e68c7935daad0c08d8c9b578e10824f218ab
utsuki-protein <utsuki.xxx@gmail.com>
2018-01-15 16:35:41 +0900

    update division func

--- calc.py
+++ calc.py
@@ -17,13 +17,11 @@ def multiplication(a, b):
 
 
 def division(a, b):
-    c = a / b
-    """
+    # c = a / b
     try:
         c = a / b
     except ZeroDivisionError as e:
         c = 99999
     except Exception as e:
         c = 99999
-    """
     return c
